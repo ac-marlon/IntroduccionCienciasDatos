ede0141b208585f1c47c2b71880767c17e16bc9b
ac-marlon <acmarlon@outlook.com>
2018-09-09 13:51:02 -0500

    Fragmenta y une aleatoriamente cualquier imagen

--- SliceConcat.py
+++ SliceConcat.py
@@ -6,6 +6,7 @@ Created on Tue Sep  4 18:54:12 2018
 """
 
 from random import shuffle
+from random import choice
 from skimage import io
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,20 +17,35 @@ matrizOriginal = (io.imread('blackWhite.png')/255.0)/255.0
 i, j = matrizOriginal.shape
 
 subMatrices = []
+divHor = []
+divVer = []
 
-for x in range(4):
-    for y in range(7):
-        subMatrices.append(matrizOriginal[(x*160):160*(x+1), (y*61):61*(y+1)])
+for n in range(2, 11):
+    if (i % n) == 0:
+        divVer.append(n)
+
+for m in range(2, 11):
+    if (j % m) == 0:
+        divHor.append(m)
+
+nPartVer = choice(divVer)
+nPartHor = choice(divHor)
+
+for x in range(nPartVer):
+    for y in range(nPartHor):
+        subMatrices.append(matrizOriginal[(x*int(i/nPartVer)):(int(i/nPartVer)*(x+1)),
+                                          (y*int(j/nPartHor)):(int(j/nPartHor)*(y+1))])
 
 shuffle(subMatrices)
+imaRand = np.hstack((subMatrices[0:nPartHor]))
 
-imaRand = np.vstack((np.hstack((subMatrices[0:7])),
-                     np.hstack((subMatrices[7:14])),
-                     np.hstack((subMatrices[14:21])),
-                     np.hstack((subMatrices[21:28]))))
+for p in range(1, nPartVer):
+    imaRand = np.concatenate((imaRand, np.hstack((subMatrices[(p*nPartHor):((p+1)*nPartHor)]))),
+                             axis=0)
 
 plt.imshow(matrizOriginal, vmin=0, vmax=1)
 plt.figure()
 plt.imshow(imaRand, vmin=0, vmax=1)
 
 io.imsave("imagenAleatoria.png", imaRand)
+
